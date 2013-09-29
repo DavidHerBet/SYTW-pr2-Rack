d3edf378e0672f89282725a1ef6c09f89da0ab3f
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-09-29 20:38:22 +0100

    Añadir programa que muestra el último tweet de un usuario de Twitter

---
+++ muestra_tweets.rb
@@ -0,0 +1,47 @@
+require 'rack'
+require 'twitter'
+require './configure'
+
+class MuestraTweets
+
+  def call env
+    req = Rack::Request.new(env)
+    res = Rack::Response.new 
+    res['Content-Type'] = 'text/html'
+    username = (req["user"] && req["user"] != '') ? req["user"] : ''
+    user_tweets = (!username.empty?) ? Twitter.user_timeline(username).first.text : "No se ha introducido ningún usuario"
+
+    res.write <<-"EOS"
+      <!DOCTYPE HTML>
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <title>>App MuestraTweet</title>
+        </head>
+        <body>
+          <section>
+            <h1>Bienvenidos a la aplicación MuestraTweet</h1>
+            <p>Introduciendo un usuario podremos ver su último tweet.</p>
+
+            <form action="/" method="post">
+              Introduzca un usuario de Twitter <input type="text" name="user" autofocus><br>
+              <input type="submit" value="Mostrar último tweet">
+            </form>
+          </section>
+          
+          <section>
+            <h2>Último tweet</h2>
+            #{username}<br>#{user_tweets}
+          </section>
+        </body>
+      </html>
+    EOS
+    res.finish
+  end
+end
+
+Rack::Server.start(
+  :app => MuestraTweets.new,
+  :Port => 9292,
+  :server => 'thin'
+)
